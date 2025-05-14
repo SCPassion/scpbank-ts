8ed1d948fad01c1a91068d23ecfc9617e16c0cff
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-05-14 20:51:50 +0200

    Add routing and layout components for the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router"
+import MainLayout from "./layouts/MainLayout"
+import DashboardLayout from "./layouts/DashboardLayout"
+import CreateVault from "./pages/CreateVault"
 function App() {
-  return <>hi</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<DashboardLayout />}>
+            <Route index element={<h1>Home</h1>} />
+            <Route path="/create" element={<CreateVault />} />
+          </Route>
+          <Route path="savings" element={<h1>Savings</h1>} />
+          <Route
+            path="interest"
+            element={<h1>Compound Interest Calculator</h1>}
+          />
+          <Route path="budget" element={<h1>Budget Planner</h1>} />
+          <Route path="retire" element={<h1>Retirement Saving Planner</h1>} />
+          <Route path="stock" element={<h1>Stock Market Watchlist</h1>} />
+          <Route path="investment" element={<h1>Invest Risk Simulator</h1>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
