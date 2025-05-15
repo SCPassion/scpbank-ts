9a8e34bc9d5328ef282f1adada0d6e111bb70246
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-05-15 21:37:10 +0200

    Update Header component to display user email or login prompt

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,15 @@
+import { useUserStore } from "@/store/store"
+
 export default function Header({ ...rest }) {
+  const { user } = useUserStore()
+  const emailName = user?.email?.split("@")[0] || ""
   const today = new Date()
 
   return (
     <div {...rest}>
-      <h1 className="cursor-pointer">"Login in!"</h1>
+      <h1 className="cursor-pointer">
+        {emailName === "" ? "Please login in first!" : emailName}
+      </h1>
       <p className="text-3xl font-normal text-[#9F9F9F]">{`>> ${today.getDate()}/${today.getMonth() + 1}/${today.getFullYear()}`}</p>
     </div>
   )
