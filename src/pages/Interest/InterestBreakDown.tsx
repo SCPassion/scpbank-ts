6778445ce04398605a53a4988485360a76242388
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-05-19 21:43:34 +0200

    refactor: remove debug log and enhance form action handling in InterestBreakDown component

--- src/pages/Interest/InterestBreakDown.tsx
+++ src/pages/Interest/InterestBreakDown.tsx
@@ -8,7 +8,6 @@ export default function InterestBreakDown() {
   const id = useId()
   const user = useUserStore((state) => state.user)
   const { interestRecords, setInterestRecords } = useInterestRecordsStore()
-  console.log("Interest Records:", interestRecords)
   async function fetchAllInterestRecords() {
     try {
       const { data, error }: SupabaseInterestRecordResponse = await supabase
@@ -35,15 +34,33 @@ export default function InterestBreakDown() {
     }
   }, [user])
 
+  function formAction(formData: FormData) {
+    const interestRecordId = formData.get("interestRecord")
+    if (interestRecordId !== null) {
+      if (Number(interestRecordId) === -1) {
+        console.error("No interest record selected")
+        return
+      }
+
+      const interestRecord = interestRecords.find(
+        (record) => record.id === Number(interestRecordId),
+      )
+
+      console.log(interestRecord)
+    }
+  }
   return (
     <div className="flex flex-col items-center gap-8 px-20 py-4">
       <h1 className="text-4xl font-bold">Select your prediction plan!</h1>
-      <form className="flex flex-col items-center justify-center gap-6 rounded-2xl border-4 border-green-500 bg-lime-100 p-8 shadow-lg duration-300 hover:border-8 hover:border-lime-800 hover:shadow-xl">
+      <form
+        className="flex flex-col items-center justify-center gap-6 rounded-2xl border-4 border-green-500 bg-lime-100 p-8 shadow-lg duration-300 hover:border-8 hover:border-lime-800 hover:shadow-xl"
+        action={formAction}
+      >
         <h3 className="text-3xl font-bold text-lime-800">
           Set up your investment calculation now!
         </h3>
         <select
-          name="vault"
+          name="interestRecord"
           id={id + "_select"}
           className="grow-1 bg-lime-200 px-4 py-1 text-xl placeholder:text-gray-700"
           defaultValue={-1}
