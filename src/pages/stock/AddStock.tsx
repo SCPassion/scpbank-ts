730fbc09df9693107d19c00041ebf48079fa70e0
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-05-21 22:04:06 +0200

    feat: implement stock symbol search functionality with debounced input and filtering

--- src/pages/stock/AddStock.tsx
+++ src/pages/stock/AddStock.tsx
@@ -3,10 +3,46 @@ import { useEffect, useState } from "react"
 
 export default function AddStock() {
   const [stockSymbols, setStockSymbols] = useState<SavedSymbol[] | null>(null)
+  const [query, setQuery] = useState<string>("")
+  const [matches, setMatches] = useState<SavedSymbol[]>([])
+  const [debouncedQuery, setDebouncedQuery] = useState<string>("")
 
+  // This is used to gather all the stock symbols from the API
+  // and store them in the stockSymbols state
   useEffect(() => {
     fetchStockSymbols()
   }, [])
+
+  // This makes sure the query is check once the user stops typing for 500ms
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setDebouncedQuery(query)
+    }, 500)
+
+    return () => clearTimeout(timeOut)
+  }, [query])
+
+  // This effect is used to filter the stock symbols based on user input.
+  // Runs when there is a change in the query or stockSymbols
+  useEffect(() => {
+    if (!debouncedQuery) {
+      setMatches([])
+      return
+    }
+    const q = debouncedQuery.toLowerCase()
+
+    if (stockSymbols) {
+      // Filter the stock symbols based on the query
+      const filtered = stockSymbols.filter(
+        (item) =>
+          item.symbol.toLowerCase().includes(q) ||
+          item.description.toLowerCase().includes(q),
+      )
+
+      // Limit the number of matches to 10
+      setMatches(filtered.slice(0, 10))
+    }
+  }, [debouncedQuery, stockSymbols])
 
   async function fetchStockSymbols() {
     const response = await fetch(
@@ -28,6 +64,23 @@ export default function AddStock() {
     setStockSymbols(filtered)
   }
 
-  console.log(stockSymbols)
-  return <h1>hi</h1>
+  return (
+    <form className="mx-32 my-6 flex flex-col items-center justify-center gap-6 rounded-2xl border-4 border-green-500 bg-lime-100 p-8 shadow-lg duration-300 hover:border-8 hover:border-lime-800 hover:shadow-xl">
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search stocks (e.g. AAPL, Tesla)"
+      />
+
+      <select name="stock">
+        <option value="">--Select a stock --</option>
+        {matches.map((item) => (
+          <option key={item.symbol} value={item.symbol}>
+            {item.description}
+          </option>
+        ))}
+      </select>
+    </form>
+  )
 }
