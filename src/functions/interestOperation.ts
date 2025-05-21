93c3851581995215b778309dc117d25510ea9a2b
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-05-21 20:17:16 +0200

    feat: refactor interest calculation logic into separate function for better modularity

--- src/functions/interestOperation.ts
+++ src/functions/interestOperation.ts
@@ -1,6 +1,7 @@
 import { type User } from "@supabase/supabase-js"
 import { supabase } from "@/supabase-client"
 import type {
+  InterestBreakDown,
   InterestRecord,
   SupabaseInterestRecordResponse,
 } from "@/lib/types"
@@ -57,4 +58,43 @@ export async function fetchAllInterestRecords(
       console.error("Error fetching interest records:", error.message)
     }
   }
+}
+
+export function calculateInterestBreakdown({
+  principal_amount,
+  apr,
+  time,
+  contribute_amount, // monthly contribution,
+  setChartData, // function to set chart data
+}: Omit<InterestBreakDown, "total"> & {
+  setChartData: (data: InterestBreakDown[]) => void
+}) {
+  const r = apr / 100
+  const n = 12
+  const result: InterestBreakDown[] = []
+
+  for (let year = 1; year <= time; year++) {
+    const compoundFactor = Math.pow(1 + r / n, n * year)
+    const futureValue = principal_amount * compoundFactor
+
+    const contributionFutureValue =
+      contribute_amount > 0
+        ? contribute_amount * ((compoundFactor - 1) / (r / n))
+        : 0
+
+    const total = futureValue + contributionFutureValue
+
+    result.push({
+      time: year,
+      total: parseFloat(total.toFixed(2)),
+      principal_amount: parseFloat(futureValue.toFixed(2)),
+      contribute_amount: parseFloat(contributionFutureValue.toFixed(2)),
+      apr: parseFloat(
+        (total - (principal_amount + contribute_amount * n * year)).toFixed(2),
+      ),
+    })
+  }
+
+  console.log("Yearly Breakdown:", result)
+  setChartData(result)
 }
